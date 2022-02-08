15b72b3f43b61c26e61dbe3b17259696f232a5fe
tianmm866 <tianmm866@163.com>
2022-02-08 18:24:20 +0800

    添加miniconda.sh到项目根目录

--- CI-install.sh
+++ CI-install.sh
@@ -6,8 +6,10 @@ echo "pip3 install pyflakes --cache-dir $HOME/.pip-cache"
 pip3 install pyflakes --cache-dir $HOME/.pip-cache
 
 # Conda Installation
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
+#wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+#bash miniconda.sh -b -p $HOME/miniconda
+# miniconda.sh放在鲜蘑菇根目录上
+bash /hy-tmp/miniconda.sh -b -p $HOME/miniconda
 source "$HOME/miniconda/etc/profile.d/conda.sh"
 hash -r
 conda config --set always_yes yes --set changeps1 no
