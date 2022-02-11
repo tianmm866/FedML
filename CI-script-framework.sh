f2d49de9124234bb8ae26509409672ef470a6b9a
tianmm866 <tianmm866@163.com>
2022-02-11 20:57:43 +0800

    调整参数 standlone可以控制参与的节点数量和轮数模型等，但是模型是一样的，需要自己写。distribute是分布式的运行，MPI有问题

--- CI-script-framework.sh
+++ CI-script-framework.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+#set -ex
 
 # code checking
 pyflakes .
@@ -9,15 +9,15 @@ pyflakes .
 source "$HOME/miniconda/etc/profile.d/conda.sh"
 conda activate fedml
 
-wandb login ee0b5f53d949c84cee7decbe7a629e63fb2f8408
+wandb login 8974651f1c691b45417700ea7e2c710cd265af67
 wandb off
 
 # 1. distributed base framework
 cd ./fedml_experiments/distributed/base
-sh run_base_distributed_pytorch.sh &
+bash run_base_distributed_pytorch.sh &
 cd ./../../../
 
 # 2. decentralized base framework
 cd ./fedml_experiments/distributed/decentralized_demo
-sh run_decentralized_demo_distributed_pytorch.sh &
+bash run_decentralized_demo_distributed_pytorch.sh &
 cd ./../../../
--- fedml_experiments/distributed/base/run_base_distributed_pytorch.sh
+++ fedml_experiments/distributed/base/run_base_distributed_pytorch.sh
@@ -2,6 +2,6 @@
 
 hostname > mpi_host_file
 
-mpirun -np 11 -hostfile ./mpi_host_file python3 ./main_base.py \
+mpirun -np 2 -hostfile ./mpi_host_file --allow-run-as-root python3 ./main_base.py \
   --client_number 10 \
   --comm_round 3
